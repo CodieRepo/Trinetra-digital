187da1c13452afc1e7e637a74f2a98b90eeb13db
CodieRepo <satwikpal786@gmail.com>
2026-06-07 23:59:20 +0530

    chore: remove exposed credentials and debug scripts

--- check_html.js
+++
@@ -1,6 +0,0 @@
-fetch('https://trinetradigitalsolution.com/admin')
-  .then(r => r.text())
-  .then(html => {
-    const matches = html.match(/src="\/assets\/[^"]*"/g);
-    console.log(matches);
-  });
