3c1d1c3ddc9bd99271d7d4ccce0150cd19f6e0bf
CodieRepo <satwikpal786@gmail.com>
2026-06-14 17:48:49 +0530

    chore: complete backend reset, remove CRM, server, ai modules

--- server/query-status.js
+++
@@ -1,6 +0,0 @@
-const gateway = require('./dist/whatsapp/gateway');
-setTimeout(() => {
-  console.log('--- GATEWAY CURRENT LIVE STATUS ---');
-  console.log(JSON.stringify(gateway.getWhatsAppStatus(), null, 2));
-  process.exit(0);
-}, 1000);
